a0043349747d3db64f6fae0a69abcb555194d41c
Rocopat <jbmaschio@gmail.com>
2018-02-16 21:54:40 -0200

    product view finished

--- app/src/main/java/com/rocopat/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/rocopat/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,45 @@
 package com.rocopat.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.widget.Adapter
+import com.rocopat.coderswag.Adapters.ProductsAdapter
 import com.rocopat.coderswag.R
+import com.rocopat.coderswag.Services.DataService
 import com.rocopat.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
-        intent.getStringExtra(EXTRA_CATEGORY)
+        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize >= 720){
+            spanCount = 3
+        }
+
+        val layoutManeger = GridLayoutManager(this, spanCount)
+
+
+
+
+
+        productsListView.layoutManager = layoutManeger
+        productsListView.adapter = adapter
     }
 }
